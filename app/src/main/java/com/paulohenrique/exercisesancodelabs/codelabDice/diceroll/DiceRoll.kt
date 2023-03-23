0b5d6d1a04be261a28e3e489672e994ad107242b
Paulo Henrique <34384560+paulohssouza@users.noreply.github.com>
2023-03-23 18:48:28 -0300

    1 - Implementation of images in the diceroll app according to the number drawn

--- app/src/main/java/com/paulohenrique/exercisesancodelabs/codelabDice/diceroll/DiceRoll.kt
+++ app/src/main/java/com/paulohenrique/exercisesancodelabs/codelabDice/diceroll/DiceRoll.kt
@@ -2,7 +2,7 @@ package com.paulohenrique.exercisesancodelabs.codelabDice.diceroll
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.paulohenrique.exercisesancodelabs.R
 import com.paulohenrique.exercisesancodelabs.codelabDice.Dice
@@ -14,8 +14,15 @@ class DiceRoll : AppCompatActivity() {
         val rollBtn: Button = findViewById(R.id.btn_dice_roll)
         val dice = Dice(6)
         rollBtn.setOnClickListener {
-            val resultText: TextView = findViewById(R.id.txt_dice_roll)
-            resultText.text = dice.roll().toString()
+            val diceImage: ImageView = findViewById(R.id.image_dice_roll)
+            when (dice.roll()) {
+                1 -> diceImage.setImageResource(R.drawable.dice_1)
+                2 -> diceImage.setImageResource(R.drawable.dice_2)
+                3 -> diceImage.setImageResource(R.drawable.dice_3)
+                4 -> diceImage.setImageResource(R.drawable.dice_4)
+                5 -> diceImage.setImageResource(R.drawable.dice_5)
+                6 -> diceImage.setImageResource(R.drawable.dice_6)
+            }
         }
     }
 }
